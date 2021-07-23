51610b0d5b255069057ee2e0b414c6e14eeca569
Raphael Marinho Teixeira <63825661+raphamt@users.noreply.github.com>
2021-07-22 23:21:39 -0300

    Update and rename Projects/gpio.s to Projects/laboratorio_03/exerc16/gpio.s

--- Projects/laboratorio_03/exerc16/gpio.s
+++ Projects/laboratorio_03/exerc16/gpio.s
@@ -8,51 +8,60 @@
         
         THUMB
 
+
+;alterando a porta N bit 12
 SYSCTL_RCGCGPIO_R       EQU     0x400FE608
 SYSCTL_PRGPIO_R		EQU     0x400FEA08
-PORTN_BIT               EQU     1000000000000b ; bit 12 = Port N
-
-GPIO_PORTN_DATA_R    	EQU     0x40064000
+PORTN_BIT               EQU     1000000000000b 
+GPIO_PORTN_DATA_R    	EQU     0x400643FC
 GPIO_PORTN_DIR_R     	EQU     0x40064400
 GPIO_PORTN_DEN_R     	EQU     0x4006451C
 
 __iar_program_start
         
-main    MOV R2, #PORTN_BIT
-	LDR R0, =SYSCTL_RCGCGPIO_R
-	LDR R1, [R0] ; leitura do estado anterior
-	ORR R1, R2 ; habilita port N
-	STR R1, [R0] ; escrita do novo estado
+;Le o estado antigo, ativa a porta N e escreve o estado novo
+main    MOV R1, #PORTN_BIT
+	LDR R4, =SYSCTL_RCGCGPIO_R
+	LDR R0, [R4] 
+	ORR R0, R1 
+	STR R0, [R4] 
 
-        LDR R0, =SYSCTL_PRGPIO_R
-wait	LDR R2, [R0] ; leitura do estado atual
-	TEQ R1, R2 ; clock do port N habilitado?
-	BNE wait ; caso negativo, aguarda
+;le o estado, verifica se o clock da porta N esta ativo e se n√£o est√° manda esperar.
+        LDR R4, =SYSCTL_PRGPIO_R
+wait	LDR R1, [R4] 
+	TEQ R0, R1 
+	BNE wait  
 
-        MOV R2, #00000001b ; bit 0
+;inicia o bit 0, le o estado antigo, da o bit de estrada e escreve o estado novo
+        MOV R1, #00000001b 
+	LDR R4, =GPIO_PORTN_DIR_R
+	LDR R0, [R4] 
+	ORR R0, R1 
+	STR R0, [R4] 
+
+;Le o estado antigo, ativa a fun√ß√£o digital e escreve o estado novo
+	LDR R4, =GPIO_PORTN_DEN_R
+	LDR R0, [R4] 
+	ORR R0, R1 
+	STR R0, [R4] 
+
+;da o primeiro estado
+        MOV R0, #000000001b 
+ 	LDR R4, = GPIO_PORTN_DATA_R
+        MOV R1, #0x3FC
+
+;o loop faz a leitura do estado da porta, muda o bit da led d2 e salva a porta alterada
+loop	
+        LDR R3, [R4] 
+        EOR R3, R0 
+        STR R3, [R4] 
         
-	LDR R0, =GPIO_PORTN_DIR_R
-	LDR R1, [R0] ; leitura do estado anterior
-	ORR R1, R2 ; bit de saÌda
-	STR R1, [R0] ; escrita do novo estado
-
-	LDR R0, =GPIO_PORTN_DEN_R
-	LDR R1, [R0] ; leitura do estado anterior
-	ORR R1, R2 ; habilita funÁ„o digital
-	STR R1, [R0] ; escrita do novo estado
-
-        MOV R1, #000000001b ; estado inicial
- 	LDR R0, = GPIO_PORTN_DATA_R
-//        MOV R2, #0x3FC
-//loop    LDR R4, [R0, R2]
-//        EOR R4, R1
-//        STR R4, [R0, R2]
-loop	STR R1, [R0, R2, LSL #2] ; aciona LED com estado atual
-        MOVT R3, #0x000F ; constante de atraso 
-delay   CBZ R3, theend ; 1 clock
-        SUB R3, R3, #1 ; 1 clock
-        B delay ; 3 clocks
-theend  EOR R1, R1, R2 ; troca o estado
+;inicia a contante de delay e faz os clocks 1 e 3
+        MOVT R3, #0x001F 
+delay   CBZ R3, theend 
+        SUB R3, R3, #1 
+        B delay ; 
+theend  
         B loop
 
         ;; Forward declaration of sections.
@@ -114,3 +123,4 @@ __default_handler
         B __default_handler
 
         END
+
